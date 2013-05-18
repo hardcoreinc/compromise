191f6bd9f486511b5bd0b70fb07f34e17a7cbbd7
Michael Pak <michaelpak@MacBook-Air-Michael.local>
2013-05-19 03:47:35 +0400

    change checker

--- compSite/checker.py
+++ compSite/checker.py
@@ -7,7 +7,8 @@ for event in mongoConnection.find({'type': 'event'}):
 	idEvent = event["_id"]
 	timeEvent = event["timestamp"]
 	deltaTime = time() - timeEvent
-	if deltaTime < 600:
-		continue
+#	if deltaTime < 600:
+#		continue
 #	for event1 in mongoConnection.find({'idEvent': str(idEvent)})
-	print json.dumps(event)
+#	print json.dumps(event)
+	print "aba"
