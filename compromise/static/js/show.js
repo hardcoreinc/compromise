39f3fe7ac7a3b05e2588192feabd87f88491e90e
russtone <antonproguitar@gmail.com>
2013-05-19 03:15:18 +0400

    show event 2

--- compromise/static/js/show.js
+++ compromise/static/js/show.js
@@ -1,6 +1,5 @@
 $(function() {
 	var tpl = _.template($('#question-div').html());
-	var json = {"timestamp": 1368910890829, "_id": "5197ec2af349c76311d2aa30", "users": [], "questions": [{"index": 0, "name": "asd2", "min": 0, "max": 100, "answers": [{"name": "123"}], "type": "list"}]}
 
 	$.each(json.questions, function(i, val) {
 		$('#questions .controls').append(tpl({data: val}));
