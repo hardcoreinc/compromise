51a03b88aa8052f40e504380cc277353c9a3d6d9
Michael Pak <michaelpak@MacBook-Air-Michael.local>
2013-05-19 03:45:02 +0400

    change checker

---
+++ compSite/checker.py
@@ -0,0 +1,13 @@
+from pymongo import Connection
+from time import time
+import json
+
+mongoConnection = Connection(host="127.0.0.1", port=27017)['compDB']['compromiseCollection']
+for event in mongoConnection.find({'type': 'event'}):
+	idEvent = event["_id"]
+	timeEvent = event["timestamp"]
+	deltaTime = time() - timeEvent
+	if deltaTime < 600:
+		continue
+#	for event1 in mongoConnection.find({'idEvent': str(idEvent)}):
+	print json.dumps(event)
