25f95b0c28a457c6ad6f73cc2f55e0aeeb7c25bf
russtone <antonproguitar@gmail.com>
2013-05-19 03:20:23 +0400

    show event 3

--- compromise/static/js/show.js
+++ compromise/static/js/show.js
@@ -12,7 +12,7 @@ $(function() {
 				json.questions[i].answers[j].current = $(a).find('input').val();
 			});
 		});
-		$.post('/addenswer/', {json: JSON.stringify(data)}).done(function(msg) {
+		$.post('/addenswer/', {json: JSON.stringify(json)}).done(function(msg) {
 			console.log(msg);
 		}).fail(function(msg) {
 			console.log(msg);
