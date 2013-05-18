268bfb1df3d430de2767566eae0121a7c27b541d
briskly <kniaz1234@gmail.com>
2013-05-19 01:09:20 +0400

    renderAnswer debuging

--- compSite/views.py
+++ compSite/views.py
@@ -62,7 +62,7 @@ def renderAnswer(request):
 	curAnswer = mongoConnection.find_one({"uniqDesc": uniqDesc})
 	idEvent = curAnswer.get("idEvent")
 	curEvent = mongoConnection.find_one({"_id": idEvent})
-	return HttpResponse(json.dumps(curEvent))
+	return HttpResponse(json.dumps([curEvent, curAnswer, idEvent]))
 
 def addAnswer(request):
 	try:
