e7c91a664312eff5b0a4156d8cd7bf08c4b8d598
Dor1s <dor3s1@gmail.com>
2013-05-19 06:53:14 +0400

    add normalizer in answers result

--- compromise/static/js/show.js
+++ compromise/static/js/show.js
@@ -10,8 +10,17 @@ $(function() {
 		var questions = $('.question-show');
 		$.each(questions, function(i,q){
 			var answers = $(q).find('.answer');
+			//normalization
+			var Normalizer = 0;
 			$.each(answers, function(j,a){
-				json.questions[i].answers[j].current = $(a).find('input').val();
+				InvertN += $(a).find('input').val();
+			});
+			Normalizer = 100/InvertN;
+			//end of normalization
+			$.each(answers, function(j,a){
+			//	json.questions[i].answers[j].current = $(a).find('input').val();	//line without normalization
+			//line with normalization:
+				json.questions[i].answers[j].current = $(a).find('input').val() / Normalizer;
 			});
 		});
 		$.post('/addanswer/', {json: JSON.stringify(json)}).done(function(msg) {
