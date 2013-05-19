1f9017e542db7ff29e443093c8281597d31f6fe1
mescheryakov <mescheryakov@rutoken.ru>
2013-05-19 09:28:34 +0400

    fix

--- compromise/urls.py
+++ compromise/urls.py
@@ -25,7 +25,7 @@ urlpatterns = patterns('',
     url(r"^addevent/", saveCompromise),
     url(r"^newevent/", newevent),
     url(r"^addanswer/", addAnswer),
-    url(r"^readyanswer/", addReady),
+    url(r"^readyanswer/", ready),
     url(r"^event/", renderAnswer),
     url(r"^$", index),
 )
