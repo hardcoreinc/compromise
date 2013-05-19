a78f4798107cefe64f3f2b3ff7529c5889847874
Michael Pak <michaelpak@MacBook-Air-Michael.local>
2013-05-19 04:08:17 +0400

    change checker

--- compSite/checker.py
+++ compSite/checker.py
@@ -3,7 +3,7 @@ from time import time
 import json
 
 mongoConnection = Connection(host="127.0.0.1", port=27017)['compDB']['compromiseCollection']
-for event in mongoConnection.find({'type': 'event'}):
+for event in mongoConnection.find({'type': 'answer'}):
 	idEvent = event["_id"]
 	timeEvent = event["timestamp"]
 	deltaTime = time() - timeEvent
